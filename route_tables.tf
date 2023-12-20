0fa706f2bd3a4160330466f0428c559069168576
amgadelhosieny <139463395+amgadelhosieny@users.noreply.github.com>
2023-12-20 20:39:53 +0200

    route_tables.tf

--- route_tables.tf
+++ route_tables.tf
@@ -4,23 +4,21 @@ resource "aws_route_table" "public" {
 }
 
 
-resource "aws_route" "public_route" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
+resource "aws_route" "public-route" {
+  route_table_id            = aws_route_table.public-route.id
+  destination_cidr_block    = "0.0.0.0/0"  
   gateway_id = aws_internet_gateway.mygw.id
 }
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "public-one" {
+  subnet_id      = aws_subnet.public-one.id
+  route_table_id = aws_route_table.public-route.id
 }
 
-
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "public-two" {
+  subnet_id      = aws_subnet.public-two.id
+  route_table_id = aws_route_table.public-route.id
 }
-
 
 
 
