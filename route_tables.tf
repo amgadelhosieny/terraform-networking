49461612688478d22f16ab46b1c418efc489f505
amgadelhosieny <139463395+amgadelhosieny@users.noreply.github.com>
2025-01-19 12:19:15 +0200

    Update route_tables.tf

--- route_tables.tf
+++ route_tables.tf
@@ -1,49 +1,53 @@
-resource "aws_route_table" "public-route" {
-  vpc_id = aws_vpc.myvpc.id
+resource "aws_route_table" "my-route-table" {
+    vpc_id = aws_vpc.myvpc.id
 
+    tags = {
+    Name = "my-route-table"
+    }
 }
-
 
 resource "aws_route" "public-route" {
-  route_table_id            = aws_route_table.public-route.id
-  destination_cidr_block    = "0.0.0.0/0"  
-  gateway_id = aws_internet_gateway.mygw.id
+    route_table_id            = aws_route_table.my-route-table.id
+    destination_cidr_block    = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-gw.id
 }
 
-resource "aws_route_table_association" "public-one" {
-  subnet_id      = aws_subnet.public-one.id
-  route_table_id = aws_route_table.public-route.id
-}
-
-resource "aws_route_table_association" "public-two" {
-  subnet_id      = aws_subnet.public-two.id
-  route_table_id = aws_route_table.public-route.id
+resource "aws_route_table_association" "public1" {
+    subnet_id      = aws_subnet.public1.id
+    route_table_id = aws_route_table.my-route-table.id
 }
 
 
-
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.myvpc.id
-
+resource "aws_route_table_association" "public2" {
+    subnet_id      = aws_subnet.public2.id
+    route_table_id = aws_route_table.my-route-table.id
 }
 
 
-resource "aws_route" "private_route" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.my_nat.id
+######################## PRIVATE ROUTE #############################
+
+
+resource "aws_route_table" "my-private-route-table" {
+    vpc_id = aws_vpc.myvpc.id
+
+    tags = {
+    Name = "my-private-route-table"
+    }
 }
 
-
+resource "aws_route" "private-route" {
+    route_table_id            = aws_route_table.my-route-table.id
+    destination_cidr_block    = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.my-nat-gw.id
+}
 
 resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private.id
+    subnet_id      = aws_subnet.private1.id
+    route_table_id = aws_route_table.my-private-route-table.id
 }
 
 
 resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.private.id
+    subnet_id      = aws_subnet.private2.id
+    route_table_id = aws_route_table.my-private-route-table.id
 }
