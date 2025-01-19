88bc31e20ad4aa7bc4d91b0c37a4bcb97c669d4d
amgadelhosieny <139463395+amgadelhosieny@users.noreply.github.com>
2025-01-19 12:21:33 +0200

    Update natgw.tf

--- natgw.tf
+++ natgw.tf
@@ -1,8 +1,14 @@
-resource "aws_eip" "nat_ip" {
+resource "aws_eip" "Nat_IP" {
 }
 
 
-resource "aws_nat_gateway" "my_nat" {
-  allocation_id = aws_eip.nat_ip.id
-  subnet_id     = aws_subnet.public1.id
+resource "aws_nat_gateway" "my-nat-gw" {
+    allocation_id = aws_eip.Nat_IP.id
+    subnet_id     = aws_subnet.public1.id
+
+    tags = {
+    Name = "GW NAT"
+    }
+
+    depends_on = [aws_internet_gateway.my-gw]
 }
