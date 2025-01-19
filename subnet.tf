5a2c4aa7aed01a8962fde8b375f66f2e6f4ecd60
amgadelhosieny <139463395+amgadelhosieny@users.noreply.github.com>
2025-01-19 12:17:33 +0200

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,24 +1,37 @@
 resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch= "true"
-}
+    vpc_id     = aws_vpc.myvpc.id
+    cidr_block = var.public_subnet_1_cidr
+    map_public_ip_on_launch = true
 
+    tags = {
+    Name = "public1"
+    }
+}
 
 resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.2.0/24"
-  map_public_ip_on_launch= "true"
-}
+    vpc_id     = aws_vpc.myvpc.id
+    cidr_block = var.public_subnet_2_cidr
+    map_public_ip_on_launch = true
 
+    tags = {
+    Name = "public2"
+    }
+}
 
 resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.3.0/24"
+    vpc_id     = aws_vpc.myvpc.id
+    cidr_block = var.private_subnet_1_cidr
+
+    tags = {
+    Name = "private1"
+    }
 }
 
-
 resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.4.0/24"
+    vpc_id     = aws_vpc.myvpc.id
+    cidr_block = var.private_subnet_2_cidr
+
+    tags = {
+    Name = "private2"
+    }
 }
